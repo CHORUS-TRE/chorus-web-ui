8e5fadea6c2d86f506ed4535d966fcac01ee7591
pcito <paloma.cito@chuv.ch>
2023-12-19 19:11:17 +0100

    see if change in website is correctly deployed

--- frontend/___tests__/Home.test.tsx
+++ frontend/___tests__/Home.test.tsx
@@ -4,6 +4,6 @@ import Home from '../src/pages/index'
 describe('Home', () => {
   it('heading', () => {
     render(<Home />)
-    const heading = screen.getByRole('heading', { level: 2, name: /Welcome to My Frontend App/i });
+    const heading = screen.getByRole('heading', { level: 2, name: /Welcome to The Frontend App/i });
     expect(heading).toBeTruthy();  })
 })
