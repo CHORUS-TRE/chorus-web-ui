856cc979103d65bef8a8b03352a3783837c30c3f
Manuel Spuhler <manuel.spuhler@gmail.com>
2024-01-14 22:15:55 +0000

    feat: background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="w-screen h-screen">
-        <nav className="absolute top-1/2"><Sidebar /></nav>
+      <main className="w-screen h-screen flex items-center border-yellow-400 border-solid border-0 overflow-hidden">
+        <Sidebar />
 
-        <iframe width="100%" className="w-screen h-screen" src="http://10.241.147.130:10000/" title="Workbench" allow="clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+        <img src="/workbench-placeholder-1024-1024.jpg" alt="workbench" className="w-full  h-full" />
+        
+        {/* <iframe width="100%" className="w-screen h-screen" src="http://10.241.147.130:10000/" title="Workbench" allow="clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe> */}
       </main>
     </div>
   )
