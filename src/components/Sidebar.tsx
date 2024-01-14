7464973a87a91c36d44bb0056e79efd33afdee3c
Manuel Spuhler <manuel.spuhler@gmail.com>
2024-01-14 22:16:18 +0000

    feat: sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,38 +1,44 @@
-import { Sidebar } from 'flowbite-react'
 import { useState } from 'react'
-import { HiArrowSmRight, HiChartPie, HiShoppingBag, HiUser, HiOutlineChevronDoubleLeft, HiOutlineChevronDoubleRight } from 'react-icons/hi'
+import { HiArrowSmRight, HiChartPie, HiUser, HiOutlineChevronDoubleLeft, HiOutlineChevronDoubleRight } from 'react-icons/hi'
 
 export default function Component() {
-  const [showSidebar, setShowSidebar] = useState(false)
+  const [showSidebar, setShowSidebar] = useState(true)
 
   return (
-    <div className="flex items-center">
-      <div className={`flex-auto w-[35vw] bg-blue-600 text-white  ease-in-out duration-300 ${showSidebar ? "translate-x-0 " : "translate-x-full"
-        }`} aria-label="Horus Analytics">
-            <h3 className="mt-20 text-4xl font-semibold text-white">sidebar</h3>
-
-        {/* <Sidebar.Logo href="#" img="/favicon.ico" imgAlt="Horus">
-          Workbench
-        </Sidebar.Logo>
-        <Sidebar.Items>
-          <Sidebar.ItemGroup>
-            <Sidebar.Item href="#" icon={HiChartPie}>
-              return to Dashboard
-            </Sidebar.Item>
-            <Sidebar.Item href="#" icon={HiUser}>
-              Share Workbench
-            </Sidebar.Item>
-            <Sidebar.Item href="#" icon={HiShoppingBag}>
-              Full Screen
-            </Sidebar.Item>
-            <Sidebar.Item href="#" icon={HiArrowSmRight}>
-              Sign out
-            </Sidebar.Item>
-          </Sidebar.ItemGroup>
-        </Sidebar.Items> */}
-      </div>
+    <div
+      className={`absolute flex items-center 
+      transform transition-transform ${showSidebar ? "translate-x-0" : "-translate-x-[calc(100%-2rem)]"}
+      border-yellow-400 border-solid border-0`}
+    >
+      <div className={`flex-auto 
+        border-cyan-700 border-solid border-0 rounded-md p-3
+        bg-white bg-opacity-30 backdrop-blur-lg backdrop-filter shadow-md
+        `} aria-label="Horus Analytics">
+        <ul className="space-y-1">
+  <li>
+    <a href="" className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-700">
+      <HiChartPie />
+      <span className="text-sm font-medium"> Dashboard </span>
+    </a>
+  </li>
+  <li>
+    <a href="" className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-700">
+      <HiUser />
+      <span className="text-sm font-medium"> Share </span>
+    </a>
+  </li>
+  <li>
+    <a href="" className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-700">
+      <HiArrowSmRight />
+      <span className="text-sm font-medium"> Logout </span>
+    </a>
+  </li>
+</ul>
+</div>
       <div className="flex-wrap">
-        <button className="p-2 rounded bg-white" onClick={() => setShowSidebar(!showSidebar)}>
+        <button className="p-1 ml-1 bg-neutral-900 text-white
+          bg-opacity-60 backdrop-blur-lg backdrop-filter
+          rounded" onClick={() => setShowSidebar(!showSidebar)}>
           {showSidebar ? <HiOutlineChevronDoubleLeft /> : <HiOutlineChevronDoubleRight />}
         </button>
       </div>
