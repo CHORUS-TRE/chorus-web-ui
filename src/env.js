c659acf3719ea6d59d00d19f499761c7a16f0ec8
pcito <paloma.cito@chuv.ch>
2024-01-22 09:21:20 +0100

    removed server side

--- src/env.js
+++ src/env.js
@@ -6,9 +6,9 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
-  server: {
-    NODE_ENV: z.enum(["development", "test", "production"]),
-  },
+  // server: {
+  //   NODE_ENV: z.enum(["development", "test", "production"]),
+  // },
 
   /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,4 @@
 import { type AppType } from "next/app";
-
-// import { api } from "~/utils/api";
-
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
