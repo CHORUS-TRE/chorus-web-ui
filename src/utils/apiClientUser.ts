287389f13643e4cb68ada3df468df3c1f20a1d59
Manuel Spuhler <manuel.spuhler@gmail.com>
2024-02-07 20:04:48 +0000

    fix: dynamic API urls 2

--- src/utils/apiClientUser.ts
+++ src/utils/apiClientUser.ts
@@ -1,7 +1,12 @@
+'use client';
+
 import { Configuration, UsersApi } from '../internal/client/index';
 
 // The base URL for the API. This should be the root URL where your API is hosted.
-const apiURL = `${window.location.protocol}//${window.location.hostname}:3000`;
+let apiURL = 'http://localhost:3000';
+if (typeof window !== "undefined") {
+  apiURL = `${window.location.protocol}//${window.location.hostname}:3000`;
+}
 
 // Creating a Configuration object for the API client.
 const apiConfig = new Configuration({ basePath: apiURL });
