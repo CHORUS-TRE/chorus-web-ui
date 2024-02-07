a2af743d66c4b315dd7a53add4e52d5f904a9a18
Manuel Spuhler <manuel.spuhler@gmail.com>
2024-02-07 12:14:02 +0000

    feat: tmp api

--- src/utils/apiClientAuthentication.ts
+++ src/utils/apiClientAuthentication.ts
@@ -1,7 +1,7 @@
 import { Configuration, AuthenticationApi } from '../internal/client/index';
 
 // URL for the API
-const apiURL = "http://localhost:3000";  // Replace "url" with the actual URL of your API
+const apiURL = `${window.location.protocol}//${window.location.hostname}:3000`;  // Replace "url" with the actual URL of your API
 
 // Creating a new Configuration object for the API client
 const apiConfig = new Configuration({ basePath: apiURL });
