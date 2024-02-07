e44bc255447cba137822e8706eebd6e5d5be6400
Manuel Spuhler <manuel.spuhler@gmail.com>
2024-02-07 12:12:09 +0000

    feat: tmp dummy api

--- src/utils/apiClientAuthentication.ts
+++ src/utils/apiClientAuthentication.ts
@@ -1,7 +1,7 @@
 import { Configuration, AuthenticationApi } from '../internal/client/index';
 
 // URL for the API
-const apiURL = "https://template-backend.horus-graph.intranet.chuv/";  // Replace "url" with the actual URL of your API
+const apiURL = "http://localhost:3000";  // Replace "url" with the actual URL of your API
 
 // Creating a new Configuration object for the API client
 const apiConfig = new Configuration({ basePath: apiURL });
--- src/utils/apiClientUser.ts
+++ src/utils/apiClientUser.ts
@@ -1,7 +1,7 @@
 import { Configuration, UsersApi } from '../internal/client/index';
 
 // The base URL for the API. This should be the root URL where your API is hosted.
-const apiURL = "https://template-backend.horus-graph.intranet.chuv/"
+const apiURL = `${window.location.protocol}//${window.location.hostname}:3000`;
 
 // Creating a Configuration object for the API client.
 const apiConfig = new Configuration({ basePath: apiURL });
