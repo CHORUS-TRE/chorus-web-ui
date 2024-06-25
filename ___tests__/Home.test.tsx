5c3f6b6c5e98fa52d494f494f4c1f6a46f4f9de5
Manuel Spuhler <manuel.spuhler@chuv.ch>
2024-06-26 00:35:04 +0200

    chore: Add Jest setup file and update Jest configuration

--- ___tests__/Home.test.tsx
+++ ___tests__/Home.test.tsx
@@ -1,8 +1,16 @@
-import { expect, test } from 'vitest'
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Home from '@/app/page'
 
-test('Home', () => {
-  render(<Home />)
-  expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+
+    const heading = screen.getByRole('heading', { level: 1 })
+
+    expect(heading).toBeInTheDocument()
+  })
 })
