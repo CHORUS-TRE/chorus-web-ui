0a0033fe6cf27e5197296fadb19cd796c7163711
Manuel Spuhler <manuel.spuhler@chuv.ch>
2024-06-25 15:50:35 +0200

    refactor: reworked structure and removed unused files

--- ___tests__/Home.test.tsx
+++ ___tests__/Home.test.tsx
@@ -1,7 +1,6 @@
 import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import Home from '../src/pages/home'
-
+import Home from '@/app/page'
 
 test('Home', () => {
   render(<Home />)
