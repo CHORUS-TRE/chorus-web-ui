09d1596a76450bc917fcd95e942a6ea02fd079f7
Manuel Spuhler <manuel.spuhler@chuv.ch>
2024-07-19 16:28:27 +0200

    feat: fix dependeny injection

--- src/app/authentication/authentication-login-view-model.ts
+++ src/app/authentication/authentication-login-view-model.ts
@@ -2,20 +2,22 @@
 
 import AuthenticationRepositoryImpl from '@/data/repository/authentication-repository-impl'
 import AuthenticationMockApiDataSourceImpl from '@/data/data-source/mock-api/authentication-mock-api-data-source-impl'
+import { AuthenticationLogin } from '@/domain/use-cases/authentication/authentication-login'
 import { useAuth } from '~/components/auth-context'
 
 export default function authenticationLoginViewModel() {
   const { setAuthentication } = useAuth()
 
-  const useCase = new AuthenticationRepositoryImpl(
-    new AuthenticationMockApiDataSourceImpl()
+  const authenticationDataSource = new AuthenticationMockApiDataSourceImpl()
+  const authenticationRepository = new AuthenticationRepositoryImpl(
+    authenticationDataSource
   )
+  const useCase = new AuthenticationLogin(authenticationRepository)
 
   const login = async (username: string, password: string) => {
-    const response = await useCase.login({ username, password })
+    const response = await useCase.execute({ username, password })
 
     if (response?.error) throw new Error(response.error.message)
-
     if (response?.data.token) setAuthentication(response?.data.token)
 
     return response?.data.token
