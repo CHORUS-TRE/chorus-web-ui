22442cf2e5a7b4bc4b4cbe6047007e0af656ab6c
Manuel Spuhler <manuel.spuhler@chuv.ch>
2024-07-23 11:19:44 +0200

    feat: authentication implementation

---
+++ src/data/data-source/api/authentication-api-data-source-impl.ts
@@ -0,0 +1,27 @@
+
+import { AuthenticationDataSource } from '@/data/data-source/'
+import { AuthenticationRequest, AuthenticationResponse } from '~/domain/model'
+import { AuthenticationServiceApi } from '@/internal/client/apis/AuthenticationServiceApi'
+
+
+class AuthenticationApiDataSourceImpl implements AuthenticationDataSource {
+
+  authService = new AuthenticationServiceApi()
+
+  async login(data: AuthenticationRequest): Promise<AuthenticationResponse> {
+
+    const authenticate = await this.authService.authenticationServiceAuthenticate({ body: data })
+
+    const token = authenticate.result?.token
+    if (!token) {
+      return { data: null, error: new Error('Invalid credentials') }
+    }
+
+    return { data: { token }, error: null }
+
+
+
+  }
+}
+
+export default AuthenticationApiDataSourceImpl
