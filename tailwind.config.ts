349ea42e36e8c1296b17d984a8176c01ec69a4c0
Manuel Spuhler <manuel.spuhler@chuv.ch>
2024-09-24 10:33:57 +0200

    style: Update color variables and styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,7 +28,7 @@ const config = {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         primary: {
-          DEFAULT: 'hsl(var(--primary))',
+          DEFAULT: 'hsl(var(--primary))', //var(tw--opacity || 1))',
           foreground: 'hsl(var(--primary-foreground))'
         },
         secondary: {
