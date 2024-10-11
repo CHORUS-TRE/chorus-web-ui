e45263360c076851bec044cb0457c4ffa71d003d
Manuel Spuhler <manuel.spuhler@chuv.ch>
2024-10-11 11:08:12 +0200

    feat: add onClick prop to Button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -7,6 +7,7 @@ export function Button({
   disabled = false,
   type = 'button',
   className,
+  onClick,
   ...props
 }: Readonly<{
   children: React.ReactNode
@@ -14,6 +15,7 @@ export function Button({
   disabled?: boolean
   type?: 'button' | 'submit' | 'reset'
   className?: string
+  onClick?: () => void
 }>) {
   return (
     <UIButton
@@ -21,6 +23,7 @@ export function Button({
       disabled={disabled}
       type={type}
       className={`flex items-center justify-start gap-1 rounded-full bg-accent text-sm text-black transition-[gap] duration-500 ease-in-out hover:gap-2 hover:bg-accent-background focus:bg-accent-background focus:ring-2 focus:ring-accent ${className}`}
+      onClick={onClick}
       {...props}
     >
       {children}
