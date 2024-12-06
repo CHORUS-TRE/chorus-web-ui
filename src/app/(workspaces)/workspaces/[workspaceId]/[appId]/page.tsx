914eef9341368e4ab28ffa21b27439aff59fb242
Manuel Spuhler <manuel.spuhler@chuv.ch>
2024-12-06 12:10:02 +0100

    refactor: removing unused state and effects

--- src/app/(workspaces)/workspaces/[workspaceId]/[appId]/page.tsx
+++ src/app/(workspaces)/workspaces/[workspaceId]/[appId]/page.tsx
@@ -8,21 +8,11 @@ import { useNavigation } from '~/components/store/navigation-context'
 import { Header } from './header'
 
 export default function WorkbenchPage() {
-  const { setBackground } = useNavigation()
-  const [show, setShow] = useState(true)
-  const params = useParams<{ workspaceId: string; appId: string }>()
-  const workbenchId = params?.appId
-  const workspaceId = params?.workspaceId
-
-  useEffect(() => {
-    if (!workbenchId || !workspaceId) return
-    setBackground(workspaceId)
-  }, [setBackground, workspaceId])
 
   return (
     <>
       <div
-        className={`absolute left-0 top-0 z-40 h-11 min-w-full transition-[top] duration-500 ease-in-out ${show ? 'top-0' : '-top-11'}`}
+        className={`absolute left-0 top-0 z-40 h-11 min-w-full transition-[top] duration-500 ease-in-out`}
         id="header"
       >
         <Header />
