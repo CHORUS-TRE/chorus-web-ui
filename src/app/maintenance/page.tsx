55d356adcaca2233eb4e25ebd68c535eaf98594b
Manuel Spuhler <manuel.spuhler@chuv.ch>
2025-07-14 14:24:33 +0200

    feat: add maintenance page component

---
+++ src/app/maintenance/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+export default function MaintenancePage() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center  animate-pulse">
+      <Image
+        src="/logo-chorus-primaire-white@2x.svg"
+        alt="Chorus"
+        width={128}
+        height={128}
+        className="mb-4"
+      />
+      <p className="text-sm text-muted">
+        We are currently performing maintenance on the system. Please check back later.
+      </p>
+    </div>
+  )
+}
