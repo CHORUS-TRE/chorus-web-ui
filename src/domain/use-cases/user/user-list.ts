10c999937eb3ad969a90e70720cc8b7c18d7e39c
Manuel Spuhler <manuel.spuhler@chuv.ch>
2025-12-03 23:45:25 +0100

    feat(user): add filtering support to listUsers function
    Add filter support (filterWorkspaceIDs) to listUsers for targeted loading:
    - Update user-data-source to accept UserServiceListUsersRequest filters
    - Update user-list use case to pass filters through the chain
    - Enable filtering users by workspace IDs for efficient data loading

--- src/domain/use-cases/user/user-list.ts
+++ src/domain/use-cases/user/user-list.ts
@@ -1,10 +1,13 @@
 import { Result, User } from '~/domain/model'
 import { UserRepository } from '~/domain/repository'
+import { UserServiceListUsersRequest } from '~/internal/client'
 
 export class UserList {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async execute(): Promise<Result<User[]>> {
-    return this.userRepository.list()
+  async execute(
+    filters: UserServiceListUsersRequest = {}
+  ): Promise<Result<User[]>> {
+    return this.userRepository.list(filters)
   }
 }
