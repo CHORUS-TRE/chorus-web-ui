0f7b7830365f35f9d843ba03c4ef324a5d4377f0
Manuel Spuhler <manuel.spuhler@chuv.ch>
2026-01-21 18:12:46 +0100

    fix: removed clientside ip anonymization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,6 @@ export default async function RootLayout({
           {`
             var _paq = window._paq = window._paq || [];
             _paq.push(['requireConsent']);
-            _paq.push(['setAnonymizeIp', true]);
             var _mtm = window._mtm = window._mtm || [];
             _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
