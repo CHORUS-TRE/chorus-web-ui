34f4b2b90b377ac688d0cd2fa54b109f422bd4f8
Jérémie Frei <jeremie.frei@chuv.ch>
2026-03-04 15:36:53 +0100

    feat: integrate useUploadWarning hook in AppStateInitializer for upload management

--- src/components/app-state-initializer.tsx
+++ src/components/app-state-initializer.tsx
@@ -2,12 +2,15 @@
 
 import { useEffect } from 'react'
 
+import { useUploadWarning } from '@/hooks/use-upload-warning'
 import { useAuthentication } from '@/providers/authentication-provider'
 import { useAppStateStore } from '@/stores/app-state-store'
 
 export function AppStateInitializer() {
   const { user } = useAuthentication()
   const initialize = useAppStateStore((state) => state.initialize)
+
+  useUploadWarning()
 
   useEffect(() => {
     initialize(user)
