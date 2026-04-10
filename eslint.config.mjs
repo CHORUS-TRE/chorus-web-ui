a51b0ca463ac1e8b1f60a17386c56ca1f72b9b07
Manuel Spuhler <manuel.spuhler@chuv.ch>
2026-04-10 21:22:11 +0200

    chore: add eslint rule preventing ~/ imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,7 +30,13 @@ export default defineConfig([
       'react-hooks/set-state-in-effect': 'off',
       'react-hooks/immutability': 'off',
       'react-hooks/purity': 'off',
-      'react-hooks/static-components': 'off'
+      'react-hooks/static-components': 'off',
+      'no-restricted-imports': ['error', {
+        patterns: [{
+          group: ['~/*'],
+          message: 'Use @/ instead of ~/. The ~/ alias has been removed.'
+        }]
+      }]
     }
   },
   {
