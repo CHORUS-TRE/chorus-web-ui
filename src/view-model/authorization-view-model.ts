3c6dd9f8d3298edc41f743f6bfbe24bdd87a6745
Manuel Spuhler <manuel.spuhler@chuv.ch>
2026-06-03 11:49:23 +0200

    feat: add authorization view model with listRoles

---
+++ src/view-model/authorization-view-model.ts
@@ -0,0 +1,19 @@
+'use client'
+
+import { env } from 'next-runtime-env'
+
+import { AuthorizationApiDataSourceImpl } from '@/data/data-source'
+import { AuthorizationRepositoryImpl } from '@/data/repository'
+import { Result } from '@/domain/model'
+import { AuthorizationRole } from '@/domain/model/authorization'
+
+const getRepository = () => {
+  const dataSource = new AuthorizationApiDataSourceImpl(
+    env('NEXT_PUBLIC_API_URL') || ''
+  )
+  return new AuthorizationRepositoryImpl(dataSource)
+}
+
+export async function listRoles(): Promise<Result<AuthorizationRole[]>> {
+  return getRepository().listRoles()
+}
